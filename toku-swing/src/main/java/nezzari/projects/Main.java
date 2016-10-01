6e8c5f3b5e940b65b75b763a24902e910ccd9273
Mustapha NEZZARI <mnezzari@gmail.com>
2016-10-01 19:45:50 +0200

    Adding sonar support + cleanup

--- toku-swing/src/main/java/nezzari/projects/Main.java
+++ toku-swing/src/main/java/nezzari/projects/Main.java
@@ -1,9 +1,22 @@
 package nezzari.projects;
 
+/**
+ * Main class
+ * 
+ * @author Mustapha NEZZARI
+ *
+ */
 public class Main {
 	
+	private Main() {}
+	
+	/**
+	 * Main function
+	 * 
+	 * @param args arguments
+	 */
 	public static void main(String[] args) {
-		
+		//TODO : To implement
 	}
 	
 }
